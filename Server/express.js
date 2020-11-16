15f9ba676db59a84076b58468f0bf5f7ef9f7fc8
Roy Ossai <royossai@maix.ovh>
2020-11-16 17:01:24 +0100

    first page

--- Server/express.js
+++ Server/express.js
@@ -1,15 +1,15 @@
 let express = require("express")
 let app = express()
+const path = require("path")
 const compression = require('compression')
 const cors = require('cors');
-const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'Client/public')));
-app.set('views', path.join(__dirname, 'Client/views'));
+app.use(express.static(path.join(__dirname, '../Client/static')));
+app.set('views', path.join(__dirname, '../Client/views'));
 app.set('view engine', 'ejs');
 
 module.exports = app;
